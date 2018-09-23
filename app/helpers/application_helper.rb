68b67bb4963fc7c11474b87f0455c97f0e76208d
Maxine Michalski <maxine_red1@yahoo.com>
2018-09-23 23:36:45 +0200

    Updated styles and added simple nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # https://gist.github.com/mynameispj/5692162
+  def current_path?(test_path)
+    return 'active' if request.path == test_path
+    ''
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  root 'welcome#index'
   get 'welcome/index'
 
-  root 'welcome#index'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
